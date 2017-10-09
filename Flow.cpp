36bcbeeb123f3da65df230111f3691761575146b
ffanzhang <ffanzhang@hotmail.com>
2017-10-09 12:06:18 -0700

    fixed rev cap should be 0, and trying some speed optimizations

--- Flow.cpp
+++ Flow.cpp
@@ -1,8 +1,14 @@
 #include <vector>
+#include <array>
 #include <queue>
 #include <iostream>
+#include <set>
+#include <cstring>
+#include <cstdio>
 
-// https://github.com/jaehyunp/stanfordacm/blob/master/code/Dinic.c://github.com/jaehyunp/stanfordacm/blob/master/code/Dinic.cc
+// based on the following
+// https://github.com/jaehyunp/stanfordacm/blob/master/code/Dinic.cc
+
 template <class I, class E>
 struct Edge {
     I u, v;
@@ -17,45 +23,52 @@ public:
     I n;
     std::vector<Edge<I, E>> Edges;
     std::vector<std::vector<I>> Graph;
-    std::vector<I> depth, pt;
-    Flow(I n) : n(n), Edges(0), Graph(n), depth(n), pt(n) {}
+    I *level;
+    I *last_visited;
+    static const E INF = (E) 0x3f3f3f3f3f3f3f3f;
+    Flow(I n) : n(n), Edges(0), Graph(n) {
+        level = new I [n];
+        last_visited = new I [n];
+    }
     void add_edge(I u, I v, E cap = 1) {
-        Edges.push_back(Edge<I, E>(u, v, cap));
-        Graph[u].push_back(Edges.size() - 1);
-        Edges.push_back(Edge<I, E>(v, u, cap));
-        Graph[v].push_back(Edges.size() - 1);
+        if (u != v) {
+            Edges.push_back(Edge<I, E>(u, v, cap));
+            Graph[u].push_back(Edges.size() - 1);
+            Edges.push_back(Edge<I, E>(v, u, 0));
+            Graph[v].push_back(Edges.size() - 1);
+        }
+    }
+    ~Flow() {
+        Edges.clear();
+        Graph.clear();
+        delete last_visited;
+        delete level;
     }
     bool bfs(I s, I t) {
+        memset(level, 0x3f, sizeof(I) * n);
         std::queue<I> q({s});
-        std::fill(depth.begin(), depth.end(), n + 1);
-        depth[s] = 0;
+        level[s] = 0;
         while (!q.empty()) {
-            I u = q.front();
-            q.pop();
+            I u = q.front(); q.pop();
             if (u == t) break;
             for (I next : Graph[u]) {
                 Edge<I, E> &edge = Edges[next];
-                if (edge.flow < edge.capacity && depth[edge.v] > depth[edge.u] + 1) {
-                    depth[edge.v] = depth[edge.u] + 1;
+                if (edge.flow < edge.capacity && level[edge.v] > level[edge.u] + 1) {
+                    level[edge.v] = level[edge.u] + 1;
                     q.push(edge.v);
                 }
             }
         }
-        return depth[t] < n + 1;
+        return level[t] < n + 1;
     }
-    E dfs(I u, I t, E flow = -1) {
+    E dfs(I u, I t, E flow) {
         if (u == t || flow == 0) return flow;
-        for (I &i = pt[u]; i < Graph[u].size(); i++) {
+        for (I &i = last_visited[u]; i < Graph[u].size(); i++) {
             Edge<I, E> &for_edge = Edges[Graph[u][i]];
             Edge<I, E> &rev_edge = Edges[Graph[u][i]^1];
-            if (depth[for_edge.v] == depth[for_edge.u] + 1) {
-                E amount = for_edge.capacity - for_edge.flow;
-                if (flow > -1 && amount > flow) {
-                    amount = flow;
-                }
-                if (E pushed = dfs(for_edge.v, t, amount)){
-                    for_edge.flow += pushed;
-                    rev_edge.flow -= pushed;
+            if (level[for_edge.v] == level[for_edge.u] + 1) {
+                if (E pushed = dfs(for_edge.v, t, std::min(flow, for_edge.capacity - for_edge.flow))) {
+                    for_edge.flow += pushed, rev_edge.flow -= pushed;
                     return pushed;
                 }
             }
@@ -65,8 +78,8 @@ public:
     E max_flow(I s, I t) {
         E total = 0;
         while (bfs(s, t)) {
-            std::fill(pt.begin(), pt.end(), 0);
-            while (E flo = dfs(s, t)) {
+            memset(last_visited, 0, sizeof(I) * n);
+            while (E flo = dfs(s, t, INF)) {
                 total += flo;
             }
         }
