62e1f8f9082d19a31ad570c0bc36954f8fb36b44
Fan Zhang <w0y6@ugrad.cs.ubc.ca>
2018-09-14 11:46:18 -0700

    edits to flow

--- Flow.cpp
+++ Flow.cpp
@@ -22,10 +22,12 @@ public:
     I n;
     std::vector<Edge<I, E>> Edges;
     std::vector<std::vector<I>> Graph;
-    I *level;
-    I *last_visited;
-    static const E INF = (E) 0x3f3f3f3f3f3f3f3f;
+    I* level;
+    I* last_visited;
+    E INF;
     Flow(I n) : n(n), Edges(0), Graph(n) {
+        Edges.reserve(2 * n - 2);
+        memset(&INF, 0x3f, sizeof(E));
         level = new I [n];
         last_visited = new I [n];
     }
@@ -50,22 +52,22 @@ public:
         while (!q.empty()) {
             I u = q.front(); q.pop();
             if (u == t) break;
-            for (I next : Graph[u]) {
-                Edge<I, E> &edge = Edges[next];
+            for (I& next : Graph[u]) {
+                Edge<I, E>& edge = Edges[next];
                 if (edge.flow < edge.capacity && level[edge.v] > level[edge.u] + 1) {
                     level[edge.v] = level[edge.u] + 1;
                     q.push(edge.v);
                 }
             }
         }
-        return level[t] < n + 1;
+        return level[t] < n;
     }
     E dfs(I u, I t, E flow) {
         if (u == t || flow == 0) return flow;
-        for (I &i = last_visited[u]; i < Graph[u].size(); i++) {
-            Edge<I, E> &for_edge = Edges[Graph[u][i]];
-            Edge<I, E> &rev_edge = Edges[Graph[u][i]^1];
-            if (level[for_edge.v] == level[for_edge.u] + 1) {
+        for (I& i = last_visited[u]; i < Graph[u].size(); i++) {
+            Edge<I, E>& for_edge = Edges[Graph[u][i]];
+            Edge<I, E>& rev_edge = Edges[Graph[u][i]^1];
+            if (level[for_edge.v] == level[for_edge.u] + 1 && for_edge.capacity > for_edge.flow) {
                 if (E pushed = dfs(for_edge.v, t, std::min(flow, for_edge.capacity - for_edge.flow))) {
                     for_edge.flow += pushed, rev_edge.flow -= pushed;
                     return pushed;
@@ -85,7 +87,3 @@ public:
         return total;
     }
 };
-
-// SPOJ FASTFLOW :AC
-// SPOJ MATCHING :TLE
-// UVA 11134 :AC
